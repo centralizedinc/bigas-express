412ad264fe8bcb57dd156569ee8352955aae3f41
abalita <abalita@centralizedinc.com>
2019-08-06 21:29:28 +0800

    update

--- src/router.js
+++ src/router.js
@@ -36,6 +36,15 @@ export default new Router({
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/Rate.vue')
     }
+    ,
+    {
+      path: '/order/track',
+      name: 'Track order',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ './views/Track.vue')
+    }
 
   ]
 })
