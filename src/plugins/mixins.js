cf8144676c84b14a211a651e9a89050cdcbab51d
Mark Jhon Paul Quijom <markjhonpaul.quijom@gmail.com>
2019-08-06 16:40:24 +0800

    first deployment

--- src/plugins/mixins.js
+++ src/plugins/mixins.js
@@ -1,60 +1,9 @@
-import service_fees from './service_fees.json'
 
 export default {
 
     install(Vue) {
         Vue.mixin({
             methods: {
-                deepCopy(obj) {
-                    return JSON.parse(JSON.stringify(obj));
-                },
-                getReferenceNumber() {
-                    var d = new Date();
-                    return Math.floor(Math.random() * 9999) +
-                        d.getFullYear().toString() +
-                        (d.getMonth() + 1).toString().length ===
-                        2
-                        ? (d.getMonth() + 1).toString()
-                        : "0" + (d.getMonth() + 1).toString() + Math.floor(Math.random() * 9999);
-                },
-                getCourse(index) {
-                    return ["Drums", "Wind", "Voice", "Piano", "Violin", "Guitar"][index]
-                },
-                getLevel(index) {
-                    return ["Primary", "Intermediate", "Advance"][index]
-                },
-                getLesson(index) {
-                    return [12, 24, 48][index]
-                },
-                getCourseCode(text) {
-                    var index = ["Drums", "Wind", "Voice", "Piano", "Violin", "Guitar"].findIndex(x => x === text);
-                    return index
-                },
-                getLevelCode(text) {
-                    var index = ["Primary", "Intermediate", "Advance"].findIndex(x => x === text);
-                    return index
-                },
-                getLessonCode(text) {
-                    var index = [12, 24, 48].findIndex(x => x === text);
-                    return index
-                },
-                getServiceFee(course, level, lesson) {
-                    console.log('service_fees :', service_fees);
-                    console.log('course :', course);
-                    console.log('level :', level);
-                    console.log('lesson :', lesson);
-                    var key = Object.keys(service_fees)[course]
-                    var fees = service_fees[key];
-
-                    var amount = fees.find(
-                        x => x.level === level && x.lesson === lesson
-                    );
-                    return amount && amount.cost ? amount.cost : null;
-                },
-                parseCurrency(x) {
-                    if (!x || isNaN(x)) return "₱ 0.00"
-                    return "₱ " + parseFloat(x).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
-                }
             }
         });
     }
--- src/store.js
+++ src/store.js
@@ -1,85 +1,13 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import api from './webview_api'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    lesson: null,
-    course: null,
-    level: null,
-    details: {
-      student: {},
-      payor: {},
-      address_details: {},
-      additional_info: "",
-      order: {}
-    },
-    sender: null
   },
   mutations: {
-    SET_LESSON(state, data) {
-      console.log('data :', data);
-      state.lesson = data
-    },
-    SET_COURSE(state, data) {
-      console.log('data :', data);
-      state.course = data
-    },
-    SET_LEVEL(state, data) {
-      console.log('data :', data);
-      state.level = data
-    },
-    SET_SENDER(state, data) {
-      console.log('data :', data);
-      state.sender = data
-    },
-    SET_ORDER(state, data) {
-      state.course = data.course;
-      state.level = data.level;
-      state.lesson = data.lesson;
-      state.sender = data.sender;
-    },
-    SET_DETAILS(state, data) {
-      state.details = data
-    },
-    RESET_DETAILS(state, data) {
-      state.lesson = null
-      state.course = null
-      state.level = null
-      state.details = {
-        student: {},
-        payor: {},
-        address_details: {},
-        additional_info: "",
-        order: {}
-      }
-    }
   },
   actions: {
-    ENROLLMENT(context, data) {
-      return new api().callbackWebview(data.sender, data.postback)
-    },
-    RESERVATION(context, data) {
-      console.log('data :', data);
-      context.commit('SET_DETAILS', data.details)
-      return new api().callbackWebview(data.sender, data.postback)
-    },
-    RESET(context, data) {
-      console.log('data :', data);
-      context.commit('RESET_DETAILS')
-      return new api().callbackWebview(data.sender, data.postback)
-    },
-    CLOSE(context, data) {
-      return new api().callbackWebview(data.sender, data.postback)
-    },
-    ASSESSMENT(context, data) {
-      return new api().callbackWebview(data.sender, data.postback)
-    },
-    SUBMIT_ENROLLMENT(context, data) {
-      console.log('SUBMIT_ENROLLMENT data :', data);
-      return new api().submitEnrollment(data)
-    }
   }
 })
