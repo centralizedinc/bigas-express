999b20055316cd801ce157caa2664969db432e66
abalita <ariel.a.balita@gmail.com>
2019-08-07 12:58:39 +0800

    update

--- src/main.js
+++ src/main.js
@@ -18,7 +18,7 @@ Vue.use(mixins);
 Vue.use(VueGeolocation);
 Vue.use(VueGoogleMaps, {
   load: {
-    key: process.env.VUE_APP_GOOGLE_MAP_KEY,
+    key: 'AIzaSyCHB6d8PoTLPZ2dtnxSPSHFpeu3pxkiA6s' || process.env.VUE_APP_GOOGLE_MAP_KEY,
   },
   installComponents: true
 })
