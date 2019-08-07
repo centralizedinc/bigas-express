5f11173714fbe9c0fb86dbeb9f56a1f1a352baa4
abalita <ariel.a.balita@gmail.com>
2019-08-07 10:16:13 +0800

    update

--- api/routes/confirmation.js
+++ api/routes/confirmation.js
@@ -5,6 +5,7 @@ const constants = require('../utils/constants_helper')
 router.route('/:sender_id')
 .post((req, res)=>{
     axios.post(
+        
         `https://graph.facebook.com/v2.6/me/messages?access_token=${constants.fb_token}`,
         {"recipient":{
             "id":req.params.sender_id
